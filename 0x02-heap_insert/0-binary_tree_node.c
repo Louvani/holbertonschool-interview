3d8344c0659028366fa362126e157b5c7837ddd3
Louvani <paula.louvani@gmail.com>
2021-12-23 18:43:30 -0500

    task 0

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -6,8 +6,9 @@
 * @value: is the value to put in the new node
 * Return: a pointer to the new node, or NULL on failure
 */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
-    binary_tree_t *new_node = NULL;
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
 
 	new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node)
