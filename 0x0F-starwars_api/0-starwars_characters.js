9f8b79cded62e22ba0d8308779b5cbbb039dbdf4
Louvani <paula.louvani@gmail.com>
2022-04-07 16:26:48 -0500

    0. Star Wars Characters

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
+
+function getName (characters, idx) {
+  if (characters.length === idx) {
+    return;
+  }
+
+  request(characters[idx], function (err, res, body) {
+    if (err) {
+      console.log(err);
+    }
+    const actor = JSON.parse(body).name;
+    console.log(actor);
+    getName(characters, ++idx);
+  });
+}
+
+request(url, function (err, res, body) {
+  if (err){
+    console.log(err);
+  }
+
+  const characters = JSON.parse(body).characters;
+  getName(characters, 0);
+});
