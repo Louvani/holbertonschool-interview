64f3ce94d091ffd83693a5199d3213e1b02d8794
Louvani <paula.louvani@gmail.com>
2022-04-14 18:06:22 -0500

    0. Rain

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 '''0. Rain module'''
 
-from traceback import walk_stack
-
 
 def rain(walls):
     '''calculate how many square units of
     water will be retained after it rains.'''
+    if (walls == []):
+        return 0
     units = 0
     wallsLen = len(walls)
     for index in range(1, wallsLen - 1):
