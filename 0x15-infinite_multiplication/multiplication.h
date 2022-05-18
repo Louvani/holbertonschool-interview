6da20fa821b0ba3bd3f7319ea77524ea7b7c9ec7
Louvani <paula.louvani@gmail.com>
2022-05-18 18:49:20 -0500

    0. Infinite Multiplication

---
+++ 0x15-infinite_multiplication/multiplication.h
@@ -0,0 +1,13 @@
+#ifndef _MULTIPLICATION_H_
+#define _MULTIPLICATION_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int _putchar(char c);
+int _isdigit(char *n);
+int _strlen(const char *s)
+
+#endif /* _MULTIPLICATION_H_ */
