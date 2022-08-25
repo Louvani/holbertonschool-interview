11fc7a4204c5ad8291c3d8ab050a6a266a82f8ca
Louvani <paula.louvani@gmail.com>
2022-08-24 21:22:07 -0500

    Documentation

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+Module to finde the winer in the prime game
+"""
 
 
 def isWinner(x, nums):
+    """
+        Function to finde the winer in the prime game
+    """
     number = 0
     for round in range(x):
         number ^= nums[round % len(nums)]
